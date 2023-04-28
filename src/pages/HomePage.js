5a592e310958898ce5cdc0edb285c4bf72235ff4
Thomas Bryson <thomasmbryson@gmail.com>
2023-04-28 04:48:01 -0700

    updated home page p

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = () => {
                     Discover Web3 ventures<span><br />through proof of community</span>
                 </h1>
                 <div className="home-content">
-                    <p>The platform for projects and investors that value builders and contributors.</p>
+                    <p>The platform for projects and investors that value builders & contributors.</p>
                 </div>
                 <TotalMessageCount className="chart-container" />
                 <Footer/>
